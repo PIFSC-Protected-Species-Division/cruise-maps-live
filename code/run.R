81f188a687a6cccbe55c57e1251756c76240eadc
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-05-25 16:49:13 -0700

    updated log to match tasktest, tested scheduler

--- code/run.R
+++ code/run.R
@@ -10,55 +10,56 @@
 # ------ Libraries --------------------------------------------------------
 
 library(googledrive)
-# library(here)
 library(swfscDAS) #https://github.com/smwoodman/swfscDAS
-library(flextable)
+library(ggplot2)
+# library(flextable)
 
 
 # ------ USER SPECIFIED INPUTS --------------------------------------------
 
-yr <- 2017
-data_source <- 'gd' # google drive
-dates0 <- 'latest' # "all" # 'latest' #"2021-06-05",
+yr = 2017
+data_source = 'gd' # google drive
+dates0 = 'latest' # "all" # 'latest' #"2021-06-05",
 # Sys.Date(), # as.character(seq(as.Date("2022-07-30"), as.Date("2022-08-14"), by="days"))
 ship = 'OES' # 'LSK'
 leg = '00'
 
 # dir_gd_raw <- paste0('cruise-maps-live/raw_das_files/', yr)
 # specifying path this way searches through all of google drive and is kind of slow
-# alternative hard code to url. 
+# alternative hard code to url.
 if (yr == 2017){
   dir_gd_raw <- 'https://drive.google.com/drive/u/0/folders/1x4GzvtLQDGT1nA7nuAPHs5CPXxsX6Umt'
 } else if (yr == 2023){
   dir_gd_raw <- 'https://drive.google.com/drive/u/0/folders/1a0GjIQs9RUY-eVoe45K7Q4zcgwHh9J2O'
 }
 
-
-# May not be able to use here package??
-# note from EM - it actually causes issues with 
-## the tasks scheduler, which has no concept of a project root folder. 
 locations <- c(
-  'C:/users/selene.fregosi/documents/github/cruise-maps-live/'
+  'C:/users/selene.fregosi/documents/github/cruise-maps-live/',
+  'C:/users/yvonne.barkley/cruise-maps-live/'
   # '//picqueenfish/psd/crp/' # want to set up a server location? for virtual machines?
-) # others add path on thier local machine
+) # others add path on their local machine
 
 for (i in 1:length(locations)){
   if (file.exists(locations[i])) {
     dir_wd  <- locations[i]
   }
 }
-# dir_wd <- "C:/Users/liz.dawson/Work/R/GAPSurveyTemperatureMap/"
+# or specify manually
+# dir_wd <- "C:/Users/selene.fregosi/documents/github/cruise-maps-live/"
 
 # as of now, all functions sourced individually, but could source all together
-# functionNames <- list.files(pattern = '[.]R$', path = paste0(dir_wd, 'code', 
-#                                                              functions), 
+# functionNames <- list.files(pattern = '[.]R$', path = paste0(dir_wd, 'code',
+#                                                              functions),
 #                             full.names = TRUE);
 # invisible(sapply(functionNames, FUN = source))
 #
 
 # ------ Make a log file --------------------------------------------------
 
-# sink(file = paste0(dir_wd, "/outputs/", Sys.Date(), ".txt"), append=TRUE)
+# simple
+sink(paste0(dir_wd, 'outputs/run_', Sys.Date(), '.log'), append = TRUE)
+cat('\n...run started', format(Sys.time(), '%Y-%m-%d %H:%M:%S %Z'), '...\n')
+cat(' dir_wd =', dir_wd, '\n')
 
 # ------ Sign in to google drive ------------------------------------------
 
@@ -66,8 +67,6 @@ googledrive_dl <- TRUE
 googledrive::drive_deauth()
 googledrive::drive_auth()
 1 # push through autorization approval
-
-
 
 # ------ Download latest survey data --------------------------------------
 
@@ -86,26 +85,39 @@ save(dasList, file = paste0(dir_wd, 'outputs/dasList_', yr, '.Rda'))
 
 # identify which files are new/need to be processed
 idxNew = which(!(dasNames_new %in% dasNames_old))
+cat(' Processing', length(idxNew), 'new das files:\n')
 # eventually loop through all idxNew
 # for (i in 1:length(idxNew)){
 #     d = dasList[idxNew[i],]
 
-#####################
 # ### for testing ###
 i = 3
-d = dasList[idxNew[i],]
-#####################
+d = dasList[i,]
+# ###################
 
 # download new das and save to git repo
-googledrive::drive_download(file = googledrive::as_id(d$id),  
-                            overwrite = TRUE, 
+googledrive::drive_download(file = googledrive::as_id(d$id),
+                            overwrite = TRUE,
                             path = paste0(dir_wd, 'inputs/', yr, '/', d$name))
+
+
+# ------ Read and process das file ----------------------------------------
+
+dasFile = paste0(dir_wd, 'inputs/', yr, '/', d$name)
+cat(' ', d$name, '\n')
+
+# basic data checks
+df_check = das_check(dasFile, skip = 0, print.cruise.nums = FALSE)
+# read and process
+df_read = das_read(dasFile, skip = 0)
+df_proc = das_process(dasFile)
+# View(df_proc)
 
 # ------ Parse track data from das ----------------------------------------
 
 # parse on-effort segments as straight lines from Begin/Resume to End 
 source(paste0(dir_wd, 'code/functions/', 'parseTrack.R'))
-etNew = parseTrack(paste0(dir_wd, 'inputs/', yr, '/', d$name))
+etNew = parseTrack(df_proc)
 
 # combine the old vs dataframe with the new one
 outStr = paste0('outputs/compiledEffortTracks_', yr, '_leg', leg, '_', ship)
@@ -120,15 +132,15 @@ if (file.exists(paste0(dir_wd, outStr, '.Rda'))){
   et = etNew
 }
 
-save(et, etNew, file = paste0(dir_wd, outStr, '.Rda'))
+save(et, file = paste0(dir_wd, outStr, '.Rda'))
 write.csv(et, file = paste0(dir_wd, outStr, '.csv'))
-
+cat('   saved', outStr, '\n')
 
 # ------ Parse track data as points ---------------------------------------
 # alternatively, can parse individual lines to get the segments out as points
 
 source(paste0(dir_wd, 'code/functions/', 'parseTrack_asPoints.R'))
-epNew = parseTrack_asPoints(paste0(dir_wd, 'inputs/', yr, '/', d$name))
+epNew = parseTrack_asPoints(df_proc)
 
 # combine the old vs dataframe with the new one
 outStr = paste0('outputs/compiledEffortPoints_', yr, '_leg', leg, '_', ship)
@@ -137,21 +149,21 @@ if (file.exists(paste0(dir_wd, outStr, '.Rda'))){
   load(paste0(dir_wd, outStr, '.Rda'))
   # combine, remove dupes, sort by date
   ep = rbind(ep, epNew)
-  ep = unique(ep) 
-  ep = ep[order(ep$DateTime),] 
+  ep = unique(ep)
+  ep = ep[order(ep$DateTime),]
 } else {
   ep = epNew
 }
 
-save(ep, epNew, file = paste0(dir_wd, outStr, '.Rda'))
+save(ep, file = paste0(dir_wd, outStr, '.Rda'))
 write.csv(ep, file = paste0(dir_wd, outStr, '.csv'))
+cat('   saved', outStr, '\n')
 
 # ------ Extract visual sighting data -------------------------------------
 
 # do some stuff here to extract visual sighting data for the day from das
 source(paste0(dir_wd, 'code/functions/', 'extractVisualSightings.R'))
-vsNew = extractVisualSightings(paste0(dir_wd, 'inputs/', yr, '/', d$name))
-
+vsNew = extractVisualSightings(df_proc)
 
 # combine the old vs dataframe with the new one
 outStr = paste0('outputs/compiledSightings_', yr, '_leg', leg, '_', ship)
@@ -161,13 +173,15 @@ if (file.exists(paste0(dir_wd, outStr, '.Rda'))){
   # combine, remove dupes, sort by date
   vs = rbind(vs, vsNew)
   vs = unique(vs)
-  vs = vs[order(vs$DateTime),] 
+  vs = vs[order(vs$DateTime),]
 } else { # if no previous sightings file exists
   vs = vsNew
 }
 
-save(vs, vsNew, file = paste0(dir_wd, outStr, '.Rda'))
+save(vs, file = paste0(dir_wd, outStr, '.Rda'))
 write.csv(vs, file = paste0(dir_wd, outStr, '.csv'))
+cat('   saved', outStr, '\n')
+
 
 # ------ Extract acoustic detections --------------------------------------
 
@@ -236,4 +250,26 @@ source(paste0(dir_wd, 'code/functions/', 'makeSummaryTable.R'))
 # then save daily update plot as .png, .pdf, whatever else we want
 # will all be generated on the posit connect 
 
-# sink()
+# ------ Simple test outputs ----------------------------------------------
+
+# make a dummy csv and dummy plot just to confirm we can make it this far!
+s = data.frame(col1 = seq(1,5,1), col2 = seq(101, 105, 1))
+# write.csv(s, file = paste0(dir_wd, 'outputs/taskTest1_',
+# format(Sys.time(), '%Y-%m-%dT%H%M%S'), '.csv'))
+
+gg = ggplot(data = s, aes(x = col1, y = col2)) + geom_line()
+# gg
+outStr = paste0('outputs/plot_', yr, '_leg', leg, '_', ship)
+ggsave(filename = paste0(dir_wd, outStr, '.png'),
+       height = 2,
+       width = 2,
+       plot = gg,
+       dpi = 120,
+       bg = 'white',
+       device = 'png')
+cat('   saved', outStr, '\n')
+
+# ------ Close up log -----------------------------------------------------
+
+cat('...run complete', format(Sys.time(), '%Y-%m-%d %H:%M:%S %Z'), '...\n')
+sink()
