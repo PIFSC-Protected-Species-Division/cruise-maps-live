cc8ff36a3455839b902a7a55ab9823c9caf2617f
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-06-30 17:30:31 -0700

    pulled out from plotMap_v4 to stand alone

---
+++ code/functions/using.R
@@ -0,0 +1,35 @@
+using <- function(...){
+  
+  #' using
+  #' 
+  #' description: function to search/install/load needed packages
+  #' 
+  #' author: Janelle Badger janelle.badger [at] noaa.gov
+  #' date: 30 June 2023
+  #'
+  #' @param #none
+  #' @return #none
+  #' @export #none
+  #'
+  #' @examples 
+  #' using('raster')
+  #' 
+  
+  libs<-unlist(list(...))
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  n<-length(need)
+  if(n>0){
+    libsmsg<-if(n>2) paste(paste(need[1:(n-1)],collapse=", "),",",sep="") else need[1]
+    print(libsmsg)
+    if(n>1){
+      libsmsg<-paste(libsmsg," and ", need[n],sep="")
+    }
+    libsmsg<-paste("The following packages could not be found: ",libsmsg,"\n\r\n\rInstall missing packages?",collapse="")
+    if(winDialog(type = c("yesno"), libsmsg)=="YES"){       
+      install.packages(need)
+      lapply(need,require,character.only=TRUE)
+    }
+  }
+}
+  
