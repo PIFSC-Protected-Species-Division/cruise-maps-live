2b3e6cfbc9c393daf545ac7c3bdea3afc9806240
sfregosi <22456126+sfregosi@users.noreply.github.com>
2023-07-05 19:21:21 -0700

    ignore automation folder

--- code/automation/autocommit.sh
+++ code/automation/autocommit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd C:/Users/Selene.Fregosi/Documents/GitHub/cruise-maps-live
+cd C:/users/user.name/github/cruise-maps-live
 git add --all
 git commit -am "auto-commit :rocket:"
 git push origin main
--- code/automation/debug_run.bat
+++
@@ -1,1 +0,0 @@
-"C:\R\R-4.3.0\bin\Rscript.exe" "C:\Users\Selene.Fregosi\Documents\GitHub\cruise-maps-live\code\run.R"
